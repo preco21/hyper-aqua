6af6f1a7c720c8c26c99323199a2b2149673dd0e
Plusb Preco <plusb21@gmail.com>
2018-05-23 11:10:24 +0900

    Finalize colors and styles

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 'use strict';
 
 // Base colors
-const backgroundColor = 'rgba(5, 25, 35, 0.87)';
-const backgroundColorWithoutTransparency = '#051923';
-const foregroundColor = '#e1edf3';
+const backgroundColor = 'rgba(150, 204, 50, 0.87)';
+const backgroundColorWithoutTransparency = '#0a1d32';
+const foregroundColor = '#dbe9f4';
 
 // Common colors
-const red = '#ff4d80';
-const orange = '#ffd147';
-const yellow = '#ffef4d';
-const green = '#adff2f';
-const cyan = '#47edff';
-const blue = '#33ccff';
-const magenta = '#c04cfd';
+const red = '#fa5282';
+const orange = '#f09030';
+const yellow = '#fae13c';
+const green = '#89e33b';
+const cyan = '#61dfff';
+const blue = '#52bdff';
+const magenta = '#ac5ce5';
 const white = '#f1f5f7';
-const gray = '#89959b';
+const gray = '#a6b4bd';
 
 // Decorate user configuration
 exports.decorateConfig = (config) => {
@@ -33,10 +33,12 @@ exports.decorateConfig = (config) => {
 
   // eslint-disable-next-line no-restricted-properties
   return Object.assign({}, config, {
+    cursorColor: yellow,
+    cursorAccentColor: yellow,
     backgroundColor: vibrancy ? backgroundColor : backgroundColorWithoutTransparency,
     foregroundColor,
     borderColor: 'transparent',
-    cursorColor: yellow,
+    selectionColor: 'rgba(225, 237, 243, 0.3)',
     colors: {
       black: backgroundColorWithoutTransparency,
       red,
@@ -57,33 +59,53 @@ exports.decorateConfig = (config) => {
     },
     termCSS: `
       ${termCSS}
-      ::selection {
-        background: rgba(255, 255, 255, 0.15);
+      *::-webkit-scrollbar {
+        width: 4px;
+        height: 4px;
+        background-color: transparent;
       }
-      ::-webkit-scrollbar-thumb {
+      *::-webkit-scrollbar-track {
+        background-color: transparent;
+      }
+      *::-webkit-scrollbar-thumb {
         background: rgba(121, 158, 201, 0.2);
       }
-      ::-webkit-scrollbar-thumb:window-inactive {
+      *::-webkit-scrollbar-thumb:window-inactive {
         background: transparent;
       }
     `,
     css: `
       ${css}
       .hyper_main {
-        border: none !important;
-      }
-      .splitpane_divider {
-        background-color: rgba(255, 255, 255, 0.34) !important;
+        border: none;
       }
       .tab_tab {
-        border: 0;
+        border: none;
+        color: rgba(255, 255, 255, 0.2);
+      }
+      .tab_tab::before {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 2px;
+        background-color: white;
+        transform: scaleX(0);
+        transition: none;
+      }
+      .tab_tab.tab_active {
+        color: #FFF;
+      }
+      .tab_tab.tab_active::before {
+        transform: scaleX(1);
+        transition: all 300ms cubic-bezier(0.0, 0.0, 0.2, 1)
       }
       .tabs_title, .tabs_list {
         font-weight: bold;
       }
-      .tab_textActive {
-        box-sizing: content-box;
-        border-bottom: 2px solid ${white};
+      .splitpane_divider {
+        background-color: rgba(0, 0, 0, 0.2) !important;
       }
     `,
     hyperStatusLine: statusLine
